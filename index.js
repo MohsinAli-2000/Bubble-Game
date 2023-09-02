9c2476c1c4ac4bbc35c4756d08c4214249fd2e77
Mohsin Ali <141604446+MohsinAli-2000@users.noreply.github.com>
2023-09-03 01:46:44 +0500

    Add files via upload
    Bubble-Game using Html,CSS and Javascript.

---
+++ index.js
@@ -0,0 +1,52 @@
+var timer=6;
+var score=0;
+var randomNumber;
+
+function makebubbles(){
+    var bubbles="";
+for(i=1; i<=176; i++){
+    var number=Math.floor(Math.random()*10)
+    bubbles += `<div class="bubble">${number}</div>`
+}
+document.querySelector("#bubble-box").innerHTML=bubbles;
+}
+makebubbles();
+
+function runtimer(){
+    var timerdown=setInterval(function(){
+        if(timer>0){
+            timer--;
+        }
+        else{
+            clearInterval(timerdown);
+            document.querySelector("#bubble-box").innerHTML="<h1>Game Over!</h1>";
+        }
+        
+        document.querySelector("#timer").textContent=timer;
+    },1000)
+}
+
+function getNewHit(){
+    randomNumber=Math.floor(Math.random()*10);
+    document.querySelector("#rn").textContent=randomNumber;
+}
+
+function increaseScore(){
+    score+=10;
+    document.querySelector("#score").textContent=score;
+       
+    }
+
+    document.querySelector("#bubble-box").addEventListener('click',function(dets){
+        var clickedNumber=Number(dets.target.textContent);
+        if (clickedNumber===randomNumber){
+            increaseScore();
+            makebubbles();
+            getNewHit();
+        }
+    })   
+    
+
+runtimer();
+getNewHit();
+
